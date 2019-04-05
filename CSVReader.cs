1d33b0c179cb77209b595024080a990fc68bf906
Sumit Kumar Das <summit.dash01@gmail.com>
2019-04-06 01:45:27 +0530

    initial upload

---
+++ CSVReader.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileReaderConsoleApp
+{
+    public sealed class CSVReader
+    {
+        private readonly string filePath = null;
+        public CSVReader()
+        {
+            string dir = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
+            filePath = dir + @"\DataFiles\sample_data.csv";
+        }
+
+        /// <summary>
+        /// Read CSV Data
+        /// </summary>
+        public List<string> ReadCSVData()
+        {
+            using (var reader = new StreamReader(this.filePath))
+            {
+                List<string> searchList = new List<string>();
+                while (!reader.EndOfStream)
+                {
+                    var line = reader.ReadLine();
+                    Console.WriteLine(line);
+                    searchList.Add(line);
+                }
+                return searchList;
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileReaderConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CSVReader csvReader = new CSVReader();
+            List<string> asd = csvReader.ReadCSVData();
+
+            Console.ReadLine();
+        }
+    }
+}
